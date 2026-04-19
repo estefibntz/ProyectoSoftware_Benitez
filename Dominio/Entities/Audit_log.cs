b73c615088fdebce51f2742bf116e3f0f75c44a7
Estefania Benitez <estefaniaabenitezz@gmail.com>
2026-04-19 16:33:18 -0300

    feat=CrearEntidad Audit_log y campos

--- Dominio/Entities/Audit_log.cs
+++ Dominio/Entities/Audit_log.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Dominio.Entities
 {
-    internal class Audit_log
+    public class Audit_log
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Action { get; set; }
+        public string EntityType { get; set; }
+        public string EntityId { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedAt { get; set; }
+
     }
 }
