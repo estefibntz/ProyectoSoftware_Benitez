80b1a1e44c54f49a7c6138e52a08a82c75cf8a4a
Estefania Benitez <estefaniaabenitezz@gmail.com>
2026-04-20 16:11:52 -0300

    feat=Registro DbContext en Program.cs y config ConnectionString

--- ProyectoSoftware_Benitez/Program.cs
+++ ProyectoSoftware_Benitez/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Infraestructura.Persistence;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));//Permite que el DbContext se cree y se concecte a la bd
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
